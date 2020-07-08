d89859d6c21d85737309cf8ee0d84c321fc64b82
eheinrich <eheinrichjosties@gmail.com>
2020-07-08 11:51:43 -0700

    Fix chunk name

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (help.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Help.vue'),
+    component: () => import(/* webpackChunkName: "help" */ '../views/Help.vue'),
     meta: {
       title: 'Help'
     }
