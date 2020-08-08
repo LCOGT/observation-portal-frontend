59aef85e57ffc9e94dd7228cacf7cfd8928e0e56
eheinrich <eheinrichjosties@gmail.com>
2020-08-07 17:20:07 -0700

    Add form for login to login template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,27 @@
 <template>
   <b-row>
-    <b-col>
-      <div id="loaded-content"></div>
+    <b-col id="form-content">
+      <div id="form-alert"></div>
+      <b-form id="obs-portal-log-in-form">
+        <b-form-group id="input-group-login-username" label="Username" label-for="input-login-username">
+          <b-form-input
+            id="input-login-username"
+            v-model="formData.username"
+            name="username"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-login-password" label="Password" label-for="input-login-password">
+          <b-form-input
+            id="input-login-password"
+            v-model="formData.password"
+            name="password"
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Log in</b-button>
+      </b-form>
       <!-- TODO: Translate this -->
       <p>Forgot your password?<router-link :to="{name: 'passwordReset'}">Reset it</router-link>.</p>
       <!-- TODO: Translate this -->
@@ -17,17 +37,21 @@ export default {
   name: 'Login',
   data: function() {
     return {
+      formData: {
+        username: '',
+        password: ''
+      },
+      // Both the element ID of this form and the ID of the form that is returned
+      // from the login procedure when there are errors
       formElementSelector: '#obs-portal-log-in-form'
     }
   },
   mounted: function() {
     let that = this;
-    let loadedContentElement = $('#loaded-content');
-    loadedContentElement.submit(function(evt) {
+    $('#form-content').submit(function(evt) {
       evt.preventDefault();
       that.submitForm();
     });
-    loadedContentElement.load(this.observationPortalLoginUrl + ' ' + this.formElementSelector);
   },
   computed: {
     observationPortalLoginUrl: function() {
