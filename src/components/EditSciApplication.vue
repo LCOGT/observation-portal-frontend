f257702a489da52048ac0e97d255455e419f30da
Jashandeep Sohi <jsohi@lco.global>
2023-05-15 09:44:06 -0700

    fix edge case with empty observing budget entries

--- src/components/EditSciApplication.vue
+++ src/components/EditSciApplication.vue
@@ -765,7 +765,7 @@ export default {
       let coInvestigators = this.getCoInvestigatorsForSubmissionBody();
       let timeRequests = this.getTimeRequestsForSubmissionBody();
       for (let i in timeRequests) {
-        if (timeRequests[i].instrument_types.some(x => x === "")) {
+        if (timeRequests[i].instrument_types.some(x => !x)) {
           continue;
         }
         formData.append('timerequest_set[' + i + ']instrument_types', timeRequests[i].instrument_types);
