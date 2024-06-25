80b7a96d3e583c2cace8b4360a824dc0aa783ef3
Joey Chatelain <32683393+jchate6@users.noreply.github.com>
2024-06-24 17:31:49 -0700

    Update Compose.vue
    change `targetLookup.text` to let the user know what target is found.

--- src/views/Compose.vue
+++ src/views/Compose.vue
@@ -958,6 +958,7 @@ export default {
             target.meandist = _.get(data, ['semimajor_axis'], null);
             target.meananom = _.get(data, ['mean_anomaly'], null);
             target.dailymot = _.get(data, ['mean_daily_motion'], null);
+            this.targetLookup.text = `Found matching Object: ${target.name}`
           } else {
             this.targetLookup.text = 'Could not find any matching objects';
             this.targetLookup.failed = true;
